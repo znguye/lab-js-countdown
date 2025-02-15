69853e7d57402bd3daaada12e601b35429863231
znguye <dnguyen.jmblc@gmail.com>
2025-02-15 18:01:25 +0100

    stop watch with toast messages

--- src/index.js
+++ src/index.js
@@ -3,36 +3,54 @@ let remainingTime = DURATION; // Countdown starting from 10
 let timer = null; // Variable to store the interval
 
 
-
 // ITERATION 1: Add event listener to the start button
-
-// Your code goes here ...
-
-
+const startButton = document.getElementById("start-btn");
+startButton.addEventListener("click",() => {
+  showToast("⏰ Final countdown! ⏰");
+  startCountdown();
+});
 
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
   console.log("startCountdown called!");
-
-
-  // Your code goes here ...
+  document.getElementById("start-btn").disabled = true;
+  timer = setInterval(() =>{
+    remainingTime--;
+    document.getElementById("time").innerText = remainingTime;
+    if(remainingTime === 5){
+      showToast("Start the engines! 💥");
+    } else if(remainingTime === 0){
+      clearInterval(timer);
+      showToast("Lift off! 🚀");
+    }
+  },1000);
 }
-
-
 
 
 // ITERATION 3: Show Toast
+
 function showToast(message) {
-  console.log("showToast called!");
+  const toast = document.getElementById("toast");
+  const toastMessage = document.getElementById("toast-message");
+  toastMessage.innerText = message;
+  toast.classList.add("show");
 
-  // Your code goes here ...
-
-
-
+  setTimeout(() => {
+    clearInterval(toast);
+    toast.classList.remove("show");
+  }, 3000);
 
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
-
-  // Your code goes here ...
-
+    const closeButton = document.getElementById("close-toast");
+    if(closeButton){
+      closeButton.addEventListener("click", () => {
+        clearInterval(toast);
+        toast.classList.remove("show");
+        console.log("Interval cleared after clicking!");
+      });
+    }else{
+      console.log("No close button found!");
+    } 
 }
+
